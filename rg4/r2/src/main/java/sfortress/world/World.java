860098a626533bfa5994ccf3a3f2a21335b0e477
John Kayne <johnhoganuke2@gmail.com>
2017-03-06 13:12:18 -0500

    Update World.java

--- rg4/r2/src/main/java/sfortress/world/World.java
+++ rg4/r2/src/main/java/sfortress/world/World.java
@@ -1,11 +1,23 @@
 package sfortress.world;
 
 public class World {
+  public String name;
   public Chunk[][][] chunks;
   public int scale;
   public int cScale;
   
-  public World() {
-    
+  public World(Object l1,String name,int scale,int cScale) {
+    pause(l1);
+  }
+  
+  public void pause(Object o) {
+    synchronized(o) {
+      o.wait();
+    }
+  }
+  public void unpause(Object o) {
+    synchronized(this) {
+      o.unpause();
+    }
   }
 }
