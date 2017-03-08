ab1c0f3e9577b9348fdf9b9235e55f8965772ad0
John Kayne <johnhoganuke2@gmail.com>
2017-03-08 11:58:15 -0500

    Update AirDuct.java

--- rg4/r2/examples/game/data/tiles/AirDuct.java
+++ rg4/r2/examples/game/data/tiles/AirDuct.java
@@ -3,5 +3,6 @@ package game.data.tiles;
 public class AirDuct extends ConveyorTile<Gas> {
   public String id = "tiles.logistics.air-duct";
   public String name = "Air-Duct";
-  
+  public String _icon = id;
+  public float maxVolume = 1000000f;
 }
