b1e2b0c38a7db9b05dfc585dffb0043a4f7ea279
John Kayne <johnhoganuke2@gmail.com>
2017-03-08 11:28:16 -0500

    Update Circuit.java

--- rg4/r2/src/main/java/sfortress/circuit/Circuit.java
+++ rg4/r2/src/main/java/sfortress/circuit/Circuit.java
@@ -1,6 +1,22 @@
 package sfortress.circuit;
 
 public class Circuit {
+  public String name;
   public ArrayList<CircuitNode> nodes = new ArrayList<CircuitNode>();
   public ArrayList<Var> vars = new ArrayList<Var>();
+  
+  public Circuit(String name) {
+    this.name = name;
+  }
+  
+  public Var getVar(String name) {
+    Var v = null;
+    for (int i = 0; i < vars.length; i++) {
+      if (vars.get(i).name == name) {
+        v = vars.get(i);
+        break;
+      }
+    }
+    return v;
+  }
 }
