4106ea6e7efebdd0604b988703e322f5b18bcff3
John Kayne <johnhoganuke2@gmail.com>
2017-03-09 12:33:26 -0500

    Create SubstanceDataPanel.java

---
+++ rg4/r2/src/main/java/sfortress/window/component/SubstanceDataPanel.java
@@ -0,0 +1,16 @@
+public class SubstanceDataPanel extends JPanel {
+  public JLabel name;
+  public JLabel _name;
+  public JLabel type;
+  public JLabel _type;
+  public JLabel meltingPoint;
+  public JLabel _meltingPoint;
+  public JLabel boilingPoint;
+  public JLabel _boilingPoint;
+  public JLabel resistivity;
+  public JLabel _resistivity;
+  public JLabel lustre;
+  public JLabel_lustre;
+  public JLabel specialProperties;
+  public JPanel _specialProperties;
+}
