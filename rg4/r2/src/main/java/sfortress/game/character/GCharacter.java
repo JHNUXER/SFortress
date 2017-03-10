3747a26f9a3d8c41469cbdfd95b282098ef0834c
John Kayne <johnhoganuke2@gmail.com>
2017-03-10 08:01:49 -0500

    Update GCharacter.java

--- rg4/r2/src/main/java/sfortress/game/character/GCharacter.java
+++ rg4/r2/src/main/java/sfortress/game/character/GCharacter.java
@@ -2,5 +2,11 @@ package sfortress.game.character;
 
 public class GCharacter {
   // Character-Specific data:
+  public Name name;
+  public Species species;
+  public Gender gender;
+  public Age age;
+  public Attributes attributes;
   // Status Data:
+  public CInventory inventory;
 }
