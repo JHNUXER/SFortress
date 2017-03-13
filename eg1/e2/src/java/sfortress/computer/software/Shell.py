9b0cd8902467dadb1d05144b9f81277d9c0079bf
John Kayne <johnhoganuke2@gmail.com>
2017-03-13 12:33:26 -0400

    Update Shell.py

--- eg1/e2/src/java/sfortress/computer/software/Shell.py
+++ eg1/e2/src/java/sfortress/computer/software/Shell.py
@@ -1,1 +1,17 @@
-class Shell:
+class Term:
+  textBuffer = [""]
+  cursorPos = [0,0]
+  
+  def write(text):
+    textBuffer[textBuffer.length-1]+=text
+  def newLine():
+    textBuffer[textBuffer.length] = ""
+  def writeLine(text):
+    write(text)
+    newLine()
+  def clear():
+    textBuffer = [""]
+  def setCursorPos(x,y):
+    self.cursorPos = [x,y]
+  def getCursorPos():
+    return self.cursorPos
